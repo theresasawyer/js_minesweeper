337b3c812d45124d78186673d483df0ab4a47e3b
tsawyer <Theresa9Sawyer@gmail.com>
2017-10-07 22:35:26 -0700

    Part one of project

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,7 +1,16 @@
 // CC intensive -- minesweeper build one 
 
-const blankLine = ' | | ';
+const blankLine = '  |   |  ';
 console.log('This is what an empty board would look like: ');
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
+
+const guessLine = '1 |   | ';
+const bombLine = '  | B | ';
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
+
